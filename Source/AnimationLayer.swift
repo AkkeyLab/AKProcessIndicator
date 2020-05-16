bbe2f185055a99beb8f1d25b354a00bb35ef0b1a
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2020-05-17 07:22:06 +0900

    :new: Create new class file

---
+++ Source/AnimationLayer.swift
@@ -0,0 +1,56 @@
+//
+//  AnimationLayer.swift
+//  AKProcessIndicator
+//
+//  Created by AKIO ITAYA on 2020/05/17.
+//  Copyright © 2020 AKIO. All rights reserved.
+//
+
+import UIKit
+
+final class AnimationLayer: CAGradientLayer {
+    internal enum Direction {
+        case right
+        case left
+    }
+
+    private enum Const {
+        static let coloredAlpha: CGFloat = 0.7
+        static let animatingDuration: CFTimeInterval = 1.5
+    }
+
+    private var direction: Direction?
+    private let animation = CABasicAnimation(keyPath: "position.x")
+
+    convenience init(_ direction: Direction) {
+        self.init()
+        self.direction = direction
+        self.masksToBounds = true
+    }
+
+    internal func addAnimation() {
+        animation.fromValue = direction == .right ? -bounds.size.width : bounds.size.width
+        animation.toValue = direction == .right ? bounds.size.width : -bounds.size.width
+        animation.duration = Const.animatingDuration
+        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        animation.timeOffset = direction == .right ? .zero : Const.animatingDuration * 0.5
+        animation.repeatCount = .infinity
+        animation.isRemovedOnCompletion = false
+        add(animation, forKey: nil)
+    }
+
+    internal func updateLayer() {
+        debugPrint(bounds.size.width)
+        if let direction = direction {
+            applyGradients(direction)
+            applyRoundCorner()
+        }
+    }
+
+    internal func updateColor(_ color: UIColor) {
+        colors = [
+            color.withAlphaComponent(.zero).cgColor,
+            color.withAlphaComponent(Const.coloredAlpha).cgColor
+        ]
+    }
+}
