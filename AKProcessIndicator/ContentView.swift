564e71d2726b890b4c2b49122a51097ec8f64d82
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2020-05-17 07:07:45 +0900

    :new: Init xcode project

---
+++ AKProcessIndicator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AKProcessIndicator
+//
+//  Created by AKIO ITAYA on 2020/05/17.
+//  Copyright © 2020 AKIO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
